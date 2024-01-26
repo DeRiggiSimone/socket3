1421c4b8feadbd3491457472dd46b7797809ab21
DeRiggiSimone <117725950+DeRiggiSimone@users.noreply.github.com>
2024-01-26 08:39:21 +0100

    Update server3.py

--- server3.py
+++ server3.py
@@ -4,6 +4,30 @@ SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
 
 def ricevi_comandi (sock_service, addr_client):
+    sock_service,addr_client=sock_server.accept()
+        with sock_service as sock_client:
+            while True:
+                data=sock_client.recv(BUFFER_SIZE)
+                print(data)
+                if not data:
+                    break
+                data=data.decode()
+                data=json.loads(data)
+                primoNumero=data['primoNumero']
+                operazione=data['operazione']
+                secondoNumero=data['secondoNumero']
+                print(primoNumero,secondoNumero,operazione)
+                if operazione=="+":
+                    ris=primoNumero+secondoNumero
+                elif operazione=="-":
+                    ris=primoNumero-secondoNumero
+                elif operazione=="*":
+                    ris=primoNumero*secondoNumero
+                elif operazione=="/":
+                    ris=primoNumero/secondoNumero
+                ris=str(ris)
+                print("risul:",ris)
+                sock_client.sendall(ris.encode())
     sock_service.close()
 
 def ricevi_connessioni (sock_listen):
@@ -12,12 +36,14 @@ def ricevi_connessioni (sock_listen):
         print("\nConnessione ricevuta da %s" % str(addr_client))
         print("Creo un thread per servire le richieste")
         try:
-            Thread(target-ricevi_comandi, args=(sock_service, addr_client)).start()
+            Thread(target=ricevi_comandi, args=(sock_service, addr_client)).start()
         except:
             print("Il thread non si avvia") 
             sock_listen.close()
     
 def avvia_server(indirizzo, porta):
+    sock_listen.bind((SERVER_IP,SERVER_PORT))
+    sock_listen.listen()
     ricevi_connessioni (sock_listen)
 if __name__=='__main__':
     avvia_server(SERVER_ADDRESS, SERVER_PORT)
