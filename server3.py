08640b47145fbd2c87942968c4ae50481a0b959a
DeRiggiSimone <117725950+DeRiggiSimone@users.noreply.github.com>
2024-01-26 09:26:03 +0100

    Update server3.py

--- server3.py
+++ server3.py
@@ -4,30 +4,34 @@ SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
 
 def ricevi_comandi (sock_service, addr_client):
-    sock_service,addr_client=sock_server.accept()
-        with sock_service as sock_client:
-            while True:
-                data=sock_client.recv(BUFFER_SIZE)
-                print(data)
-                if not data:
-                    break
-                data=data.decode()
-                data=json.loads(data)
-                primoNumero=data['primoNumero']
-                operazione=data['operazione']
-                secondoNumero=data['secondoNumero']
-                print(primoNumero,secondoNumero,operazione)
-                if operazione=="+":
-                    ris=primoNumero+secondoNumero
-                elif operazione=="-":
-                    ris=primoNumero-secondoNumero
-                elif operazione=="*":
-                    ris=primoNumero*secondoNumero
-                elif operazione=="/":
-                    ris=primoNumero/secondoNumero
-                ris=str(ris)
-                print("risul:",ris)
-                sock_client.sendall(ris.encode())
+    print("avviato") 
+    while True:
+        data=sock_service.recv(1024)
+        if not data: #se data è vuoto esce dal ciclo
+            break
+        data=data.decode()
+        data=json.loads(data)
+        primoNumero=data['primoNumero'] 
+        operazione=data['operazione'] 
+        secondoNumero=data['secondoNumero']
+        ris=""
+        if operazione=="+":
+            ris=primoNumero+secondoNumero
+        elif operazione=="-":
+            ris-primoNumero-secondoNumero
+        elif operazione=="*":
+            ris-primoNumero*secondoNumero
+        elif operazione=="/":
+            if secondoNumero==0:
+                ris="non puoi dividere per 0"
+            else:
+                ris-primoNumero/secondoNumero
+        elif operazione=="%":
+            ris=primoNumero%secondoNumero
+        else:
+            ris="Operazione non riconosciuta" 
+        ris=str(ris) #casting
+        sock_service.sendall(ris.encode("UTF-8")) #manda vettore al client
     sock_service.close()
 
 def ricevi_connessioni (sock_listen):
@@ -42,9 +46,15 @@ def ricevi_connessioni (sock_listen):
             sock_listen.close()
     
 def avvia_server(indirizzo, porta):
-    sock_listen.bind((SERVER_IP,SERVER_PORT))
-    sock_listen.listen()
-    ricevi_connessioni (sock_listen)
+    sock_listen = socket.socket()
+    sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+    sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
+    sock_listen.listen(5)
+    print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT))) 
+    ricevi_connessioni(sock_listen)
+
+
+
 if __name__=='__main__':
     avvia_server(SERVER_ADDRESS, SERVER_PORT)
 print("Termina")
